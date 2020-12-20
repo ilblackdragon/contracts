ab434b24b2b298d269054d3168beffc40e0f8e7d
Illia Polosukhin <ilblackdragon@gmail.com>
2020-12-19 17:17:46 -0800

    Adding generic factory contract

---
+++ generic-factory/src/lib.rs
@@ -0,0 +1,73 @@
+use near_lib::upgrade::Ownable;
+use near_sdk::{AccountId, env, near_bindgen, Promise};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::{Base64VecU8};
+
+#[global_allocator]
+static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc::INIT;
+
+const CODE_KEY: &[u8; 4] = b"code";
+
+/// This gas spent on the call & account creation, the rest goes to the `new` call.
+const CREATE_CALL_GAS: u64 = 5_000_000_000_000_000;
+
+#[near_bindgen]
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct GenericFactory {
+    owner: AccountId,
+}
+
+impl Default for GenericFactory {
+    fn default() -> Self {
+        env::panic(b"GenericFactory should be initialized before usage")
+    }
+}
+
+#[near_bindgen]
+impl GenericFactory {
+    #[init]
+    pub fn new(owner: AccountId, code: Base64VecU8) -> Self {
+        assert!(!env::state_exists(), "The contract is already initialized");
+        env::storage_write(CODE_KEY, &code.0);
+        Self { owner }
+    }
+
+    pub fn create(&self, name: AccountId, args: Base64VecU8) -> Promise {
+        let code = env::storage_read(CODE_KEY).expect("Code must be present");
+        Promise::new(format!("{}.{}", name, env::current_account_id()))
+            .create_account()
+            .deploy_contract(code)
+            .function_call(b"new".to_vec(), args.into(), env::attached_deposit(), env::prepaid_gas() - CREATE_CALL_GAS)
+    }
+
+    pub fn upgrade(&self, code: Base64VecU8) {
+        self.assert_owner();
+        env::storage_write(CODE_KEY, &code.0);
+    }
+}
+
+impl Ownable for GenericFactory {
+    fn get_owner(&self) -> AccountId {
+        self.owner.clone()
+    }
+    fn set_owner(&mut self, owner: AccountId) {
+        self.assert_owner();
+        self.owner = owner;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use near_lib::context::{accounts, VMContextBuilder};
+    use near_sdk::{MockedBlockchain, testing_env};
+
+    use super::*;
+
+    #[test]
+    fn test_basics() {
+        testing_env!(VMContextBuilder::new().finish());
+        let factory = GenericFactory::new(accounts(0), vec![].into());
+        assert_eq!(factory.get_owner(), accounts(0));
+        factory.create("test".to_string(), vec![].into());
+    }
+}
--- near-lib-rs/src/upgrade.rs
+++ near-lib-rs/src/upgrade.rs
@@ -4,6 +4,27 @@ use near_sdk::json_types::{Base64VecU8};
 
 use crate::types::{Duration, Timestamp, WrappedDuration};
 
+pub trait Ownable {
+    fn assert_owner(&self) {
+        assert_eq!(env::predecessor_account_id(), self.get_owner());
+    }
+    fn get_owner(&self) -> AccountId;
+    fn set_owner(&mut self, owner: AccountId);
+}
+
+pub trait Upgradable {
+    fn get_staging_duration(&self) -> WrappedDuration;
+    fn stage_code(&mut self, code: Base64VecU8, timestamp: Timestamp);
+    fn deploy_code(&mut self) -> Promise;
+
+    /// Implement migration for the next version.
+    /// Should be `unimplemented` for a new contract.
+    /// TODO: consider adding version of the contract stored in the storage?
+    fn migrate(&mut self) {
+        unimplemented!();
+    }
+}
+
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct Upgrade {
     pub owner: AccountId,
@@ -19,24 +40,25 @@ impl Upgrade {
             staging_timestamp: 0,
         }
     }
+}
 
-    pub fn assert_owner(&self) {
-        assert_eq!(env::predecessor_account_id(), self.owner);
-    }
-
-    pub fn get_owner(&self) -> AccountId {
+impl Ownable for Upgrade {
+    fn get_owner(&self) -> AccountId {
         self.owner.clone()
     }
 
-    pub fn set_owner(&mut self, owner: AccountId) {
+    fn set_owner(&mut self, owner: AccountId) {
+        self.assert_owner();
         self.owner = owner;
     }
+}
 
-    pub fn get_staging_duration(&self) -> WrappedDuration {
+impl Upgradable for Upgrade {
+    fn get_staging_duration(&self) -> WrappedDuration {
         self.staging_duration.into()
     }
 
-    pub fn stage_code(&mut self, code: Base64VecU8, timestamp: Timestamp) {
+    fn stage_code(&mut self, code: Base64VecU8, timestamp: Timestamp) {
         self.assert_owner();
         assert!(env::block_timestamp() + self.staging_duration < timestamp, "Timestamp must be later than staging duration");
         // Writes directly into storage to avoid serialization penalty by using default struct.
@@ -44,7 +66,7 @@ impl Upgrade {
         self.staging_timestamp = timestamp;
     }
 
-    pub fn deploy_code(&mut self) -> Promise {
+    fn deploy_code(&mut self) -> Promise {
         if self.staging_timestamp < env::block_timestamp() {
             env::panic(&format!("Deploy code too early: staging ends on {}", self.staging_timestamp + self.staging_duration).into_bytes());
         }
@@ -52,17 +74,4 @@ impl Upgrade {
         env::storage_remove(b"upgrade");
         Promise::new(env::current_account_id()).deploy_contract(code)
     }
-}
-
-pub trait Upgradable {
-    fn get_owner(&self) -> AccountId;
-    fn set_owner(&mut self, owner: AccountId);
-    fn get_staging_duration(&self) -> WrappedDuration;
-    fn stage_code(&mut self, code: Base64VecU8, timestamp: Timestamp);
-    fn deploy_code(&mut self) -> Promise;
-
-    /// Implement migration for the next version.
-    /// Should be empty for the new contract.
-    /// TODO: consider adding version of the contract stored in the storage?
-    fn migrate(&mut self);
 }
