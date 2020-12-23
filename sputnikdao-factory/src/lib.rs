1d899a10ea3722750b78173aed4477f0ac70bf1e
Illia Polosukhin <ilblackdragon@gmail.com>
2020-12-23 04:03:31 -0800

    Adding list of created daos in the factory

--- sputnikdao-factory/src/lib.rs
+++ sputnikdao-factory/src/lib.rs
@@ -1,6 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::Base64VecU8;
 use near_sdk::{env, near_bindgen, AccountId, Promise};
+use near_sdk::collections::UnorderedSet;
 
 #[global_allocator]
 static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc::INIT;
@@ -11,13 +12,35 @@ const CODE: &[u8] = include_bytes!("../../sputnikdao/res/sputnikdao.wasm");
 const CREATE_CALL_GAS: u64 = 30_000_000_000_000;
 
 #[near_bindgen]
-#[derive(BorshSerialize, BorshDeserialize, Default)]
-pub struct SputnikDAOFactory {}
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct SputnikDAOFactory {
+    daos: UnorderedSet<AccountId>,
+}
+
+impl Default for SputnikDAOFactory {
+    fn default() -> Self {
+        env::panic(b"SputnikDAOFactory should be initialized before usage")
+    }
+}
 
 #[near_bindgen]
 impl SputnikDAOFactory {
-    pub fn create(&self, name: AccountId, args: Base64VecU8) -> Promise {
-        Promise::new(format!("{}.{}", name, env::current_account_id()))
+    #[init]
+    pub fn new() -> Self {
+        assert!(!env::state_exists(), "The contract is already initialized");
+        Self {
+            daos: UnorderedSet::new(b"d".to_vec()),
+        }
+    }
+
+    pub fn get_dao_list(&self) -> Vec<AccountId> {
+        self.daos.to_vec()
+    }
+
+    pub fn create(&mut self, name: AccountId, args: Base64VecU8) -> Promise {
+        let account_id= format!("{}.{}", name, env::current_account_id());
+        self.daos.insert(&account_id);
+        Promise::new(account_id)
             .create_account()
             .deploy_contract(CODE.to_vec())
             .transfer(env::attached_deposit())
